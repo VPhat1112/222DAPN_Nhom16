adb5ded2bfc61c2853e7ef414e8c2098d61c886b
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-04-24 00:04:40 +0700

    tuankiet_commit1

---
+++ DAPM-Nhom16/Pages/Admin/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DAPM_Nhom16.Pages.Admin
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DAPM-Nhom16/Pages/Admin/Warehouse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DAPM_Nhom16.Pages.Admin
+{
+    public class WarehouseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
